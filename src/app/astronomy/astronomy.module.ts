768833305fe90771eecc392094c214b4179da611
Torsten Oppermann <oppermann.torsten@googlemail.com>
2021-08-19 18:40:24 +0200

    Basic potd component + basic routing

---
+++ src/app/astronomy/astronomy.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PotdComponent } from './potd/potd.component';
+
+@NgModule({
+  declarations: [
+    PotdComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AstronomyModule { }
---
+++ src/app/astronomy/potd/potd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-potd',
+  templateUrl: './potd.component.html',
+  styleUrls: ['./potd.component.scss']
+})
+export class PotdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
